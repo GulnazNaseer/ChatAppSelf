fff2cc2af318ffc0716a1adcd83e56bebce09bb6
Ahmad Ibraheem Azka <gulnaznaseer21@gmail.com>
2020-11-28 20:06:11 +0500

    Initial commit

---
+++ app/src/main/java/com/aa/autotow/chatappself/core/users/getall/GetUsersContract.java
@@ -0,0 +1,44 @@
+package com.aa.autotow.chatappself.core.users.getall;
+
+
+import com.aa.autotow.chatappself.models.User;
+
+import java.util.List;
+
+
+
+public interface GetUsersContract {
+    interface View {
+        void onGetAllUsersSuccess(List<User> users);
+
+        void onGetAllUsersFailure(String message);
+
+        void onGetChatUsersSuccess(List<User> users);
+
+        void onGetChatUsersFailure(String message);
+    }
+
+    interface Presenter {
+        void getAllUsers();
+
+        void getChatUsers();
+    }
+
+    interface Interactor {
+        void getAllUsersFromFirebase();
+
+        void getChatUsersFromFirebase();
+    }
+
+    interface OnGetAllUsersListener {
+        void onGetAllUsersSuccess(List<User> users);
+
+        void onGetAllUsersFailure(String message);
+    }
+
+    interface OnGetChatUsersListener {
+        void onGetChatUsersSuccess(List<User> users);
+
+        void onGetChatUsersFailure(String message);
+    }
+}
---
+++ app/src/main/java/com/aa/autotow/chatappself/ui/activities/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.aa.autotow.chatappself.ui.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.aa.autotow.chatappself.R;
+import com.aa.autotow.chatappself.ui.fragments.LoginFragment;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    public static void startIntent(Context context) {
+        Intent intent = new Intent(context, LoginActivity.class);
+        context.startActivity(intent);
+    }
+
+    public static void startIntent(Context context, int flags) {
+        Intent intent = new Intent(context, LoginActivity.class);
+        intent.setFlags(flags);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_activity_login);
+        bindViews();
+        init();
+    }
+
+    private void bindViews() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    private void init() {
+        // set the toolbar
+        setSupportActionBar(mToolbar);
+
+        // set the login screen fragment
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_content_login,
+                LoginFragment.newInstance(),
+                LoginFragment.class.getSimpleName());
+        fragmentTransaction.commit();
+    }
+}
